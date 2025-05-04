7d725650385930dd571e8814eaf706e648048520
ezra <ezralawrence02@gmail.com>
2025-05-04 21:22:33 +0000

    Add KDE GUI module

---
+++ modules/gui/kde.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver.enable = true;
+
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.defaultSession = "plasma";
+
+  services.xserver.videoDrivers = [ "modesetting" ]; # Change if needed
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    pulse.enable = true;
+  };
+
+  networking.networkmanager.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    kate
+    kdeconnect
+    dolphin
+    konsole
+  ];
+}
